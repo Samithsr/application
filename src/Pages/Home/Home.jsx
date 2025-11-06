d65c2b3c3aae48d39ee3dcb4191bdd679b88fcf4
Samith R Gowda <141106865+Samithsr@users.noreply.github.com>
2025-11-06 17:29:56 +0530

    adding routes and navbar will design and add state

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import react from "react"
+import './Home.css'
+
+const Home = () => {
+    return(
+        <div></div>
+    )
+}
+
+
+export default Home;
+ 
---
+++ src/Pages/PlaceOrder/PlaceOredr.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOredr = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PlaceOredr
